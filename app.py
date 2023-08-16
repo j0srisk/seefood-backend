e74a7a2b7b27ab1dbf9c8b978a3330344c1f25a2
j0srisk <j0srisk@gmail.com>
2023-08-16 02:45:40 -0500

    updated dependencies

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from transformers import ViltProcessor, ViltForQuestionAnswering
 import requests
+import time
 from PIL import Image
 
 app = Flask(__name__)
-
+CORS(app)
 API_KEY = os.environ.get("API_KEY")
 
 
@@ -13,11 +15,15 @@ API_KEY = os.environ.get("API_KEY")
 def predict():
     provided_api_key = request.headers.get("X-API-Key")
     if provided_api_key == API_KEY:
-        data = request.json
-        url = data["url"]
-        question = data["question"]
+        start_time = time.time()
 
-        image = Image.open(requests.get(url, stream=True).raw)
+        question = request.form["question"]
+        image_file = request.files["file"]
+
+        print(question)
+        print(image_file)
+
+        image = Image.open(image_file)
 
         processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
         model = ViltForQuestionAnswering.from_pretrained(
@@ -30,6 +36,15 @@ def predict():
         idx = logits.argmax(-1).item()
         answer = model.config.id2label[idx]
 
-        return jsonify({"answer": answer})
+        end_time = time.time()
+
+        processing_time = int((end_time - start_time) * 1000)
+
+        return jsonify({"answer": answer, "processing_time_ms": processing_time})
     else:
         return jsonify({"error": "Unauthorized"}), 401
+
+
+# testing the API locally
+# if __name__ == "__main__":
+# app.run(host="0.0.0.0", port=8080)
